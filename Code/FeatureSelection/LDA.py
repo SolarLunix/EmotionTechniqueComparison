9d972d75ffa46c44fbc5660b9eb7b3b8987fb6bd
Melissa <SolarLunix@gmail.com>
2018-11-12 12:40:41 +0000

    Added LDA

---
+++ Code/FeatureSelection/LDA.py
@@ -0,0 +1,6 @@
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+name = 'LDA'
+
+LDA_Set_1 = LDA(solver='svd', shrinkage=None, priors=None, n_components=None,
+                store_covariance=False, tol=0.0001)
